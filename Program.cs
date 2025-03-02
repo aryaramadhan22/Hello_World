26ae1090b0b335fb825368fd792b7a09bea6bf4f
Arya Ramadhan <5053241033@student.its.ac.id>
2025-03-02 19:27:47 +0700

    Menambahkan loop di Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// File: Program.cs
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Hello, World! Iteration {i + 1}");
+        }
+    }
+}
